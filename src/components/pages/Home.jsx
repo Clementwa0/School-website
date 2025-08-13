ee5fa725fdcb6db4832fa64a55236a326a4735ef
clementwa0 <clementwa01@gmail.com>
2025-08-14 01:12:39 +0300

    Integrate shadcn UI components, redesign navbar, update home app UI/UX

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,10 +1,41 @@
 import React from 'react'
-
+import Button from "../ui/button"
+import { Contact } from 'lucide-react'
+import { LucideCornerDownRight } from 'lucide-react'
+import { LucideCornerDownLeft } from 'lucide-react'
+import { Zap } from 'lucide-react'
 const Home = () => {
   return (
     <div className='bg-gray-200 container flex'>
         <h1>Better Future For Your Kids</h1>
         <h3>Let the child be the director, and the actor in his own play</h3>
+        <div>
+            <img src='' alt='kid1'/>
+        </div>
+        <div>
+            <Button>
+                Get Started <span></span>
+            </Button>
+        </div>
+        <div>
+            <p>lorem despasito blabla</p>
+        </div>
+
+        <div>
+            <div>
+                <Contact/> <h1>7.5k+</h1>
+                <h2>Total active students taking gifted courses</h2>
+            </div>
+        <LucideCornerDownRight/>
+        <LucideCornerDownLeft/>
+        <div>
+            <Zap/> <h1>50+</h1>
+            <h2>Available field programs and increasing</h2>
+        </div>
+        </div>
+        <div>
+            <img src='' alt='kid1'/>
+        </div>
     </div>
   )
 }
--- src/components/pages/Navbar.jsx
+++ src/components/pages/Navbar.jsx
@@ -1,8 +1,8 @@
-// src/components/Navbar.jsx
 import { Link } from "react-router-dom";
 import { Menu, X } from "lucide-react";
 import { useState } from "react";
-import { MoveDown } from "lucide-react";
+import { YoutubeIcon } from "lucide-react";
+import { ChevronDown } from "lucide-react";
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -11,8 +11,8 @@ export default function Navbar() {
 
   return (
     <nav className="bg-white fixed w-full top-0 left-0 z-50">
-      <div className="max-w-5xl shadow-md  mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-between h-16">
+      <div className="max-w-5xl shadow-md  mx-auto border-b px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16 ">
           {/* Logo */}
           <Link to="/" className="text-2xl font-bold text-slate-900">
             EduSchool
@@ -24,15 +24,19 @@ export default function Navbar() {
               Home
             </Link>
             <Link to="/about" className="text-gray-700 hover:text-white hover:bg-gray-900 p-1 rounded-md">
-              About 
+              About  <ChevronDown/>
             </Link>
             <Link to="/courses" className="text-gray-700 hover:text-white hover:bg-gray-900 p-1 rounded-md">
-              Courses
+              Courses 
             </Link>
             <Link to="/contact" className="text-gray-700 hover:text-white hover:bg-gray-900 p-1 rounded-md">
               Contact
             </Link>
           </div>
+           <Link to="/" className="text-md flex gap-1 text-slate-900">
+            Youtube <span className="bg-gray-100 text-red-600 shadow-md border border-gray-100 rounded px-1"><YoutubeIcon/></span>
+          </Link> 
+
 
           {/* Mobile Menu Button */}
           <div className="md:hidden flex items-center">
