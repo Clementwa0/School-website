6250c7f0c2fa6fcac7985a30794df7c490c49781
clementwa0 <clementwa01@gmail.com>
2025-08-14 00:45:40 +0300

    Add school website navigation links

---
+++ src/constants/index.js
@@ -0,0 +1,9 @@
+export const links = [
+  { path: "/", name: "Home" },
+  { path: "/about", name: "About Us" },
+  { path: "/admissions", name: "Admissions" },
+  { path: "/academics", name: "Academics" },
+  { path: "/facilities", name: "Facilities" },
+  { path: "/events", name: "Events" },
+  { path: "/contact", name: "Contact Us" }
+];
