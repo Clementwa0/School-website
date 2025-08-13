f367f1a62ee907bc7a8547d4beba9e66bf1a3fc3
clementwa0 <clementwa01@gmail.com>
2025-08-14 00:44:29 +0300

    Export Navbar and Home components

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='bg-gray-200 container flex'>
+        <h1>Better Future For Your Kids</h1>
+        <h3>Let the child be the director, and the actor in his own play</h3>
+    </div>
+  )
+}
+
+export default Home
